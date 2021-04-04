ca7cdc6341ee572589081367ceee1434eb7f0b9c
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-04-04 18:26:55 +0200

    test

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicHVja2IiLCJhIjoiY2ttbHR0MmNnMDE3eTJucGdzemRmdmV1NiJ9.AXo5MlyGCM9BEfuXWdJajQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-96, 37.8],
+  zoom: 3
+});
