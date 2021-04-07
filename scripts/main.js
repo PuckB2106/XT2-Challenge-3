7517d1bee077bd29d2a316eb15dbd1240395f2f8
PuckB2106 <78908678+PuckB2106@users.noreply.github.com>
2021-04-07 20:11:53 +0200

    opentripmap toegevoegd

--- scripts/main.js
+++ scripts/main.js
@@ -4,38 +4,208 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicHVja2IiLCJhIjoiY2ttbHR0MmNnMDE3eTJucGdzemRmd
 // Initialate map
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/satellite-v9',
+style: 'mapbox://styles/mapbox/streets-v11',
 center: [4.322840, 52.067101],
 zoom: 11.15
 });
 
+// var geocoder = new MapboxGeocoder({
+// accessToken: mapboxgl.accessToken,
+// mapboxgl: mapboxgl
+// })
+
+//get coords zoekbalk
 var geocoder = new MapboxGeocoder({
-accessToken: mapboxgl.accessToken,
-mapboxgl: mapboxgl
-})
-
-map.on('load', function () {
-// Listen for the `geocoder.input` event that is triggered when a user
-// makes a selection
-geocoder.on('result', function (ev) {
-console.log(ev.result.center);
-
- });
+    accessToken: mapboxgl.accessToken,
+    types: 'country,region,place,postcode,locality,neighborhood'
 });
 
-document.getElementById('citybutton').onclick = function(){
-            	getAPIdata();
-            }
+// place with id
+var features = [];
 
-            // init data stream
-            var geocoder = new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            });
+geocoder.addTo('#geocoder');
 
-            // Voeg de zoekbalk toe
-            map.addControl( geocoder, 'top-left');
+// Add geocoder result to container.
+geocoder.on('result', function (e) {
+    let coords = e.result.geometry.coordinates;
 
+    //update map, go to coordinates
+    map.flyTo({
+        center: coords,
+        zoom: 15
+    });
+
+    features = [];
+    getBars(coords);
+    // getCultural(coords);
+});
+
+//andere api  parameter coords
+function getBars(coords) {
+    const openTripMapKey = '5ae2e3f221c38a28845f05b6a22cdc4a0cfb0a132d606ec9a0834be3';
+    let url = 'https://api.opentripmap.com/0.1/en/places/radius',
+        qString = '?radius=1500&lon=' + coords[0] + '&lat=' + coords[1] + '&kinds=bars&limit=25&apikey=' + openTripMapKey;
+
+    fetch(url + qString)
+        .then(resp => {
+            return resp.json();
+        }).then(data => {
+        let bars = data.features;
+
+        for (let i = 0; i < bars.length; i++) {
+            let bar = bars[i];
+
+            let obj = {};
+            obj.id = bar.id;
+            obj.type = 'Feature';
+            obj.properties = {};
+            obj.properties.description = '<strong>' + bar.properties.name + '</strong>';
+            obj.properties.icon = 'bar';
+            obj.geometry = {};
+            obj.geometry.type = 'Point';
+            obj.geometry.coordinates = bar.geometry.coordinates;
+
+            features.push(obj);
+        }
+        placeMarkers();
+    }).catch((error) => {
+        alert(error);
+    })
+}
+
+//markers gemaakt
+function placeMarkers() {
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+    });
+
+    // Add a layer showing the places.
+    map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+            'icon-image': '{icon}-15',
+            'icon-size': 2,
+            'icon-allow-overlap': true
+        }
+    });
+
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    map.on('mouseenter', 'places', function (e) {
+
+        const openTripMapKey = '5ae2e3f221c38a28845f05b65370e244b3b310f07de647889ddf591c';
+        let url = 'https://api.opentripmap.com/0.1/en/places/xid/' + e.features[0].id,
+            qString = '?apikey=' + openTripMapKey;
+        fetch(url + qString)
+            .then(resp => {
+                return resp.json();
+            }).then(data => {
+            let address = '<p>' + data.address.road + ' ' + data.address.house_number + '<br>' + data.address.postcode + ' ' + data.address.city + '<br>' + data.address.country + '</p>';
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = e.features[0].properties.description + address;
+
+            // Populate the popup and set its coordinates based on the feature found.
+            popup.setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        }).catch((error) => {
+            alert(error);
+        })
+    });
+
+    map.on('mouseleave', 'places', function () {
+        popup.remove();
+    });
+}
+
+//markers gemaakt
+function placeMarkers() {
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+    });
+
+    // Add a layer showing the places.
+    map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+            'icon-image': '{icon}-15',
+            'icon-size': 2,
+            'icon-allow-overlap': true
+        }
+    });
+
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    map.on('mouseenter', 'places', function (e) {
+
+        const openTripMapKey = '5ae2e3f221c38a28845f05b65370e244b3b310f07de647889ddf591c';
+        let url = 'https://api.opentripmap.com/0.1/en/places/xid/' + e.features[0].id,
+            qString = '?apikey=' + openTripMapKey;
+        fetch(url + qString)
+            .then(resp => {
+                return resp.json();
+            }).then(data => {
+            let address = '<p>' + data.address.road + ' ' + data.address.house_number + '<br>' + data.address.postcode + ' ' + data.address.city + '<br>' + data.address.country + '</p>';
+            var coordinates = e.features[0].geometry.coordinates.slice();
+            var description = e.features[0].properties.description + address;
+
+            // Populate the popup and set its coordinates based on the feature found.
+            popup.setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(map);
+        }).catch((error) => {
+            alert(error);
+        })
+    });
+
+    map.on('mouseleave', 'places', function () {
+        popup.remove();
+    });
+}
+
+
+
+
+// map.on('load', function () {
+// // Listen for the `geocoder.input` event that is triggered when a user
+// // makes a selection
+// geocoder.on('result', function (ev) {
+// console.log(ev.result.center);
+//
+//  });
+// });
+//
+// document.getElementById('citybutton').onclick = function(){
+//             	getAPIdata();
+//             }
+//
+//             // init data stream
+//             var geocoder = new MapboxGeocoder({
+//                 accessToken: mapboxgl.accessToken,
+//                 mapboxgl: mapboxgl
+//             });
+//
+//             // // Voeg de zoekbalk toe
+//             // map.addControl( geocoder, 'top-left');
+//
             map.on('load', function () {
             	// Listen for the `geocoder.input` event that is triggered when a user
             	// makes a selection
